e42a0e06a540d3a2d9bbf101c27e812bcd6430e9
dflynn-code <dflynn@wbsedu.de>
2026-02-25 15:18:31 +0100

    Update libft.h

--- includes/libft.h
+++ includes/libft.h
@@ -1,10 +1,6 @@
-#ifndef PROJECT_LIBFT_WRAPPER_H
-# define PROJECT_LIBFT_WRAPPER_H
+#ifndef LIBFT_WRAPPER_H
+# define LIBFT_WRAPPER_H
 
-/*
-** Public libft entrypoint for cub3d sources.
-** Project headers/sources must include "libft.h" from includes/ only.
-*/
 # include "../libft/libft.h"
 
 #endif
