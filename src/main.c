6f3ef2c9c640f7a8be470af794c0a2af5af7e571
dflynn-code <dflynn@wbsedu.de>
2026-02-25 10:40:55 +0100

    Include libft in the shared cub3d header

--- src/main.c
+++ src/main.c
@@ -1,92 +1,55 @@
-//remember sig for later
+#include "cub3d.h"
 
-#include "mlx.h"
-#include <stdlib.h>
-#include <math.h>
-
-void draw_background(void *mlx, void *win, int screenWidth, int screenHeight);
-
-int main(void)
+void	init_game(t_game *game)
 {
-    void *mlx;
-    void *win;
-    int screenWidth = 200;
-    int screenHeight = 100;
+	game->screen_width = 200;
+	game->screen_height = 100;
+	game->pos_x = 12.0;
+	game->pos_y = 12.0;
+	game->dir_x = -1.0;
+	game->dir_y = 0.0;
+	game->plane_x = 0.0;
+	game->plane_y = 0.66;
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		exit(1);
+	game->win = mlx_new_window(game->mlx, game->screen_width,
+			game->screen_height, "Cub3D Test Wall");
+	if (!game->win)
+		exit(1);
+}
 
-    mlx = mlx_init();
-    if (!mlx)
-        return (1);
-    win = mlx_new_window(mlx, screenWidth, screenHeight, "Cub3D Test Wall");
-    if (!win)
-        return (1);
+void	init_world_map(t_game *game)
+{
+	int	i;
+	int	j;
 
-    double posX = 12.0, posY = 12.0; // move player away from edges
-    double dirX = -1.0, dirY = 0.0;
-    double planeX = 0.0, planeY = 0.66;
+	i = 0;
+	while (i < 24)
+	{
+		j = 0;
+		while (j < 24)
+		{
+			game->world_map[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	i = 0;
+	while (i < 24)
+	{
+		game->world_map[10][i] = 1;
+		i++;
+	}
+}
 
-    int worldMap[24][24] = {0};
+int	main(void)
+{
+	t_game	game;
 
-    // Create a full wall row in front of the player
-    for (int i = 0; i < 24; i++)
-        worldMap[10][i] = 1;  // row 10 is a wall
-
-    draw_background(mlx, win, screenWidth, screenHeight);
-
-    // simple raycasting loop to draw red wall
-    for (int x = 0; x < screenWidth; x++)
-    {
-        double cameraX = 2 * x / (double)screenWidth - 1;
-        double rayDirX = dirX + planeX * cameraX;
-        double rayDirY = dirY + planeY * cameraX;
-
-        int mapX = (int)posX;
-        int mapY = (int)posY;
-
-        double deltaDistX = (rayDirX == 0) ? 1e30 : fabs(1 / rayDirX);
-        double deltaDistY = (rayDirY == 0) ? 1e30 : fabs(1 / rayDirY);
-
-        int stepX = (rayDirX < 0) ? -1 : 1;
-        int stepY = (rayDirY < 0) ? -1 : 1;
-
-        double sideDistX = (rayDirX < 0) ? (posX - mapX) * deltaDistX : (mapX + 1.0 - posX) * deltaDistX;
-        double sideDistY = (rayDirY < 0) ? (posY - mapY) * deltaDistY : (mapY + 1.0 - posY) * deltaDistY;
-
-        int hit = 0, side;
-        while (hit == 0)
-        {
-            if (sideDistX < sideDistY)
-            {
-                sideDistX += deltaDistX;
-                mapX += stepX;
-                side = 0;
-            }
-            else
-            {
-                sideDistY += deltaDistY;
-                mapY += stepY;
-                side = 1;
-            }
-            if (worldMap[mapX][mapY] > 0)
-                hit = 1;
-        }
-
-        double perpWallDist = (side == 0) ? (mapX - posX + (1 - stepX) / 2) / rayDirX
-                                         : (mapY - posY + (1 - stepY) / 2) / rayDirY;
-
-        int lineHeight = (int)(screenHeight / perpWallDist);
-        int drawStart = -lineHeight / 2 + screenHeight / 2;
-        if (drawStart < 0) drawStart = 0;
-        int drawEnd = lineHeight / 2 + screenHeight / 2;
-        if (drawEnd >= screenHeight) drawEnd = screenHeight - 1;
-
-        int color = 0xFF0000;
-        if (side == 1)
-            color /= 2;
-
-        for (int y = drawStart; y < drawEnd; y++)
-            mlx_pixel_put(mlx, win, x, y, color);
-    }
-
-    mlx_loop(mlx);
-    return (0);
+	init_game(&game);
+	init_world_map(&game);
+	render_frame(&game);
+	mlx_loop(game.mlx);
+	return (0);
 }
--- src/raycasting.c
+++ src/raycasting.c
@@ -10,21 +10,109 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mlx.h"
+#include "cub3d.h"
 
-// raycasting.c
-#include "mlx.h"
-
-void draw_background(void *mlx, void *win, int screenWidth, int screenHeight)
+void	draw_background(t_game *game)
 {
-    for (int y = 0; y < screenHeight; y++)
-    {
-        for (int x = 0; x < screenWidth; x++)
-        {
-            if (y < screenHeight / 2)
-                mlx_pixel_put(mlx, win, x, y, 0x87CEEB); // ceiling: light blue
-            else
-                mlx_pixel_put(mlx, win, x, y, 0x228B22); // floor: forest green
-        }
-    }
+	int	x;
+	int	y;
+
+	y = 0;
+	while (y < game->screen_height)
+	{
+		x = 0;
+		while (x < game->screen_width)
+		{
+			if (y < game->screen_height / 2)
+				mlx_pixel_put(game->mlx, game->win, x, y, 0x87CEEB);
+			else
+				mlx_pixel_put(game->mlx, game->win, x, y, 0x228B22);
+			x++;
+		}
+		y++;
+	}
+}
+
+void	render_frame(t_game *game)
+{
+	int		x;
+	int		map_x;
+	int		map_y;
+	double	camera_x;
+	double	ray_dir_x;
+	double	ray_dir_y;
+	double	delta_dist_x;
+	double	delta_dist_y;
+	double	side_dist_x;
+	double	side_dist_y;
+	double	perp_wall_dist;
+	int		step_x;
+	int		step_y;
+	int		hit;
+	int		side;
+	int		line_height;
+	int		draw_start;
+	int		draw_end;
+	int		y;
+	int		color;
+
+	draw_background(game);
+	x = 0;
+	while (x < game->screen_width)
+	{
+		camera_x = 2 * x / (double)game->screen_width - 1;
+		ray_dir_x = game->dir_x + game->plane_x * camera_x;
+		ray_dir_y = game->dir_y + game->plane_y * camera_x;
+		map_x = (int)game->pos_x;
+		map_y = (int)game->pos_y;
+		delta_dist_x = (ray_dir_x == 0) ? 1e30 : fabs(1 / ray_dir_x);
+		delta_dist_y = (ray_dir_y == 0) ? 1e30 : fabs(1 / ray_dir_y);
+		step_x = (ray_dir_x < 0) ? -1 : 1;
+		step_y = (ray_dir_y < 0) ? -1 : 1;
+		side_dist_x = (ray_dir_x < 0) * (game->pos_x - map_x) * delta_dist_x
+			+ (ray_dir_x >= 0) * (map_x + 1.0 - game->pos_x) * delta_dist_x;
+		side_dist_y = (ray_dir_y < 0) * (game->pos_y - map_y) * delta_dist_y
+			+ (ray_dir_y >= 0) * (map_y + 1.0 - game->pos_y) * delta_dist_y;
+		hit = 0;
+		while (hit == 0)
+		{
+			if (side_dist_x < side_dist_y)
+			{
+				side_dist_x += delta_dist_x;
+				map_x += step_x;
+				side = 0;
+			}
+			else
+			{
+				side_dist_y += delta_dist_y;
+				map_y += step_y;
+				side = 1;
+			}
+			if (game->world_map[map_x][map_y] > 0)
+				hit = 1;
+		}
+		if (side == 0)
+			perp_wall_dist = (map_x - game->pos_x + (1 - step_x) / 2)
+				/ ray_dir_x;
+		else
+			perp_wall_dist = (map_y - game->pos_y + (1 - step_y) / 2)
+				/ ray_dir_y;
+		line_height = (int)(game->screen_height / perp_wall_dist);
+		draw_start = -line_height / 2 + game->screen_height / 2;
+		if (draw_start < 0)
+			draw_start = 0;
+		draw_end = line_height / 2 + game->screen_height / 2;
+		if (draw_end >= game->screen_height)
+			draw_end = game->screen_height - 1;
+		color = 0xFF0000;
+		if (side == 1)
+			color /= 2;
+		y = draw_start;
+		while (y < draw_end)
+		{
+			mlx_pixel_put(game->mlx, game->win, x, y, color);
+			y++;
+		}
+		x++;
+	}
 }
