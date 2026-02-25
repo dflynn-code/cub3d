25d39b5a994303bf3de9738e9b0378e2c525aee7
dflynn-code <dflynn@wbsedu.de>
2026-02-25 09:18:07 +0100

    Refactor raycasting demo around shared game state

---
+++ includes/cub3d.h
@@ -0,0 +1,28 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include "mlx.h"
+# include <math.h>
+# include <stdlib.h>
+
+typedef struct s_game
+{
+	void	*mlx;
+	void	*win;
+	int		screen_width;
+	int		screen_height;
+	double	pos_x;
+	double	pos_y;
+	double	dir_x;
+	double	dir_y;
+	double	plane_x;
+	double	plane_y;
+	int		world_map[24][24];
+}t_game;
+
+void	init_game(t_game *game);
+void	init_world_map(t_game *game);
+void	render_frame(t_game *game);
+void	draw_background(t_game *game);
+
+#endif
